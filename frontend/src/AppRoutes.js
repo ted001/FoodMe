76c44d11709332014c1508489d78cf743fdac881
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2023-10-27 20:32:18 -0700

    Add: thumb nails component

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,11 +1,11 @@
-import { Route } from "react-router-dom";
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
 
-export default function AppRoutes(){
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-        </Routes>
-    );
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
