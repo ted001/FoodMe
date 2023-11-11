fe6db98188566f29df7ebaa447fbf80e9eb2446a
Zhiyi Jin <victoryjzy@gmail.com>
2023-11-11 15:34:22 -0800

    Add Payment page

--- frontend/src/hooks/useCart.js
+++ frontend/src/hooks/useCart.js
@@ -1,5 +1,4 @@
-import React, {createContext, useState, useContext} from "react";
-import { useEffect } from "react";
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const CartContext = createContext(null);
 const CART_KEY = 'cart';
@@ -10,77 +9,85 @@ const EMPTY_CART = {
 };
 
 export default function CartProvider({ children }) {
-    const initCart = getCartFromLocalStorage();
-    const [cartItems, setCartItems] = useState(initCart.items);
+  const initCart = getCartFromLocalStorage();
+  const [cartItems, setCartItems] = useState(initCart.items);
   const [totalPrice, setTotalPrice] = useState(initCart.totalPrice);
   const [totalCount, setTotalCount] = useState(initCart.totalCount);
 
-    useEffect(() => {
-        const totalPrice = sum(cartItems.map(item => item.price));
-        const totalCount = sum(cartItems.map(item => item.quantity));
-        setTotalPrice(totalPrice);
-        setTotalCount(totalCount);
+  useEffect(() => {
+    const totalPrice = sum(cartItems.map(item => item.price));
+    const totalCount = sum(cartItems.map(item => item.quantity));
+    setTotalPrice(totalPrice);
+    setTotalCount(totalCount);
 
-        localStorage.setItem(
-            CART_KEY,
-            JSON.stringify({
-              items: cartItems,
-              totalPrice,
-              totalCount,
-            })
-          );
-      }, [cartItems]);
+    localStorage.setItem(
+      CART_KEY,
+      JSON.stringify({
+        items: cartItems,
+        totalPrice,
+        totalCount,
+      })
+    );
+  }, [cartItems]);
 
-
-    function getCartFromLocalStorage() {
+  function getCartFromLocalStorage() {
     const storedCart = localStorage.getItem(CART_KEY);
     return storedCart ? JSON.parse(storedCart) : EMPTY_CART;
-    }
+  }
 
-    const sum = items => {
-        return items.reduce((prevValue, curValue) => prevValue + curValue, 0);
-    };
+  const sum = items => {
+    return items.reduce((prevValue, curValue) => prevValue + curValue, 0);
+  };
 
-    const removeFromCart = foodId => {
-        const filteredCartItems = cartItems.filter(item => item.food.id !== foodId);
-        setCartItems(filteredCartItems);
-      };
+  const removeFromCart = foodId => {
+    const filteredCartItems = cartItems.filter(item => item.food.id !== foodId);
+    setCartItems(filteredCartItems);
+  };
 
-    const changeQuantity = (cartItem, newQauntity) => {
+  const changeQuantity = (cartItem, newQauntity) => {
     const { food } = cartItem;
 
     const changedCartItem = {
-        ...cartItem,
-        quantity: newQauntity,
-        price: food.price * newQauntity,
+      ...cartItem,
+      quantity: newQauntity,
+      price: food.price * newQauntity,
     };
 
     setCartItems(
-        cartItems.map(item => (item.food.id === food.id ? changedCartItem : item))
+      cartItems.map(item => (item.food.id === food.id ? changedCartItem : item))
     );
-    };
+  };
 
-    const addToCart = food => {
-        const cartItem = cartItems.find(item => item.food.id === food.id);
-        if (cartItem) {
-          changeQuantity(cartItem, cartItem.quantity + 1);
-        } else {
-          setCartItems([...cartItems, { food, quantity: 1, price: food.price }]);
-        }
-      };
+  const addToCart = food => {
+    const cartItem = cartItems.find(item => item.food.id === food.id);
+    if (cartItem) {
+      changeQuantity(cartItem, cartItem.quantity + 1);
+    } else {
+      setCartItems([...cartItems, { food, quantity: 1, price: food.price }]);
+    }
+  };
 
-    return (
-        <CartContext.Provider
-          value={{
-            cart: { items: cartItems, totalPrice, totalCount }, 
-            removeFromCart,
-            changeQuantity,
-            addToCart
-          }}
-        >
-          {children}
-        </CartContext.Provider>
-      );
-      
+  const clearCart = () => {
+    localStorage.removeItem(CART_KEY);
+    const { items, totalPrice, totalCount } = EMPTY_CART;
+    setCartItems(items);
+    setTotalPrice(totalPrice);
+    setTotalCount(totalCount);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart: { items: cartItems, totalPrice, totalCount },
+        removeFromCart,
+        changeQuantity,
+        addToCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
+
 export const useCart = () => useContext(CartContext);
