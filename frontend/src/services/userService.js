884351ea0c6ca53cf289ce41b8956fb98bbbabe3
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2023-11-14 10:32:07 -0800

    Add: profile page more

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -15,8 +15,18 @@ export const login = async (email, password) => {
     const { data } = await axios.post('api/users/login', { email, password });
     localStorage.setItem('user', JSON.stringify(data));
     return data;
-}
+};
 
 export const logout = () => {
     localStorage.removeItem('user');
-}
+};
+
+export const updateProfile = async user => {
+    const { data } = await axios.put('/api/users/updateProfile', user);
+    localStorage.setItem('user', JSON.stringify(data));
+    return data;
+  };
+  
+  export const changePassword = async passwords => {
+    await axios.put('/api/users/changePassword', passwords);
+  };
